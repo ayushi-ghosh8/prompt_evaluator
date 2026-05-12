b195b045f8786fcf79409dbd7613113ea55045ed
Ayushi <ghoshayushi218@gmail.com>
2026-05-12 13:36:54 +0530

    Fixed api routing

--- backend/main.py
+++ backend/main.py
@@ -22,8 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#app.include_router(api_router, prefix="/api")
-app.include_router(api_router)
+app.include_router(api_router, prefix="/api")
 
 @app.get("/")
 def root():
